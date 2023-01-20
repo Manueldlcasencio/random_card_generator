bbcad3d09bf3092072a249bad32fe697781be0d3
Manueldlcasencio <manuel.dlcasencio@gmail.com>
2023-01-20 19:52:23 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function card() {
+  let palo = ["spade", "heart", "club", "diamond"];
+  let ran = Math.floor(Math.random() * 4);
+  let num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let ran2 = Math.floor(Math.random() * 12);
+  document.body.innerHTML =
+    '<div class="card ' + palo[ran] + '">' + num[ran2] + "</div>";
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  card();
+  setInterval(card, 2000);
 };
